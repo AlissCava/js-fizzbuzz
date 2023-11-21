344e2f983c35b1f648e28378ebaf701e7a7cff48
AlissCava <cavalleraalissa@gmail.com>
2023-11-21 17:23:20 +0100

    fine es base

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-// Stampa i numeri da 1 a 100 con le regole di FizzBuzz
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
+// Funzione per stampare i numeri da 1 a 100 con le regole di FizzBuzz
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
     }
 }
+
+// Chiamata alla funzione al caricamento della pagina
+fizzBuzz();
